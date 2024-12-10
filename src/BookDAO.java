73d938e1de2ca706f23503cba6ca69cad8637a47
mouse <kufreedward26@gmail.com>
2024-12-10 13:55:26 +0100

    implemented database query method

--- src/BookDAO.java
+++ src/BookDAO.java
@@ -1,6 +1,12 @@
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBDeleteExpression;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression;
+import com.amazonaws.services.dynamodbv2.datamodeling.PaginatedQueryList;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class BookDAO {
 
@@ -20,8 +26,18 @@ public class BookDAO {
      * @return the PaginatedQueryList that is returned from the query
      */
     public List<Book> getBooksReadByEmployee(String employeeId) { //(Book book) {
-        //TODO: implement
-        return null;
+
+        Book book = new Book();
+        book.setId(employeeId);
+
+
+        DynamoDBQueryExpression<Book> dynamoDBQueryExpression = new DynamoDBQueryExpression<Book>()
+                .withHashKeyValues(book);
+
+        PaginatedQueryList<Book> bookList = mapper.query(Book.class, dynamoDBQueryExpression);
+
+
+        return bookList;
     }
 
 }
